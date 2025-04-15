5633f07a1087b96a8699315efc5c7842639534ad
John Vincent Peralta <87950013+JVP02@users.noreply.github.com>
2025-04-15 09:39:42 +0800

    Add files via upload

---
+++ comp/header.php
@@ -0,0 +1,29 @@
+<header>
+        <div class="header-top">
+            <img src="asset/logorec.png" alt="logo" class="logo-img">
+            <div class="header-menu">
+                <ul class="menu-list">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Video</a></li>
+                    <li><a href="">Category</a></li>
+                    <li><a href="">Live</a></li>
+                    <li><a href="">Community</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </div>
+            <div class="medium-btn">
+                <button class="btn-primary" type="button">Login</button>
+                <button class="btn-secondary" type="button">Sign Up</button>
+            </div>
+        </div>
+    </header>
+
+    <aside class="sidebar" id="sidebar">
+        <ul class="sidebar-menu">
+            <li><a href="#"><img src="asset/i/home.png" alt="Home Icon" class="menu-icon"> <span class="menu-text">Dashboard</span></a></li>
+            <li><a href="#"><img src="asset/i/play.png" alt="Play Icon" class="menu-icon"> <span class="menu-text">My Videos</span></a></li>
+            <li><a href="#"><img src="asset/i/category.png" alt="Category Icon" class="menu-icon"> <span class="menu-text">Subscriptions</span></a></li>
+            <li><a href="#"><img src="asset/i/settings.png" alt="Settings Icon" class="menu-icon"> <span class="menu-text">Settings</span></a></li>
+            <li><a href="#"><img src="asset/i/logout.png" alt="Logout Icon" class="menu-icon"> <span class="menu-text">Logout</span></a></li>
+        </ul>
+    </aside>
